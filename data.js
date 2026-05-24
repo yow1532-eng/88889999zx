7a84024ead05e934474dcea5128cc1301cbef7b4
yow1532-eng <yow1532@gmail.com>
2026-05-24 19:02:58 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+// ✏️ 게시글을 여기에 추가하세요
+// thumb: 갤러리에 보일 대표 썸네일 이미지
+// images: 게시글 안에 들어갈 사진들 (여러 장 가능)
+// 이미지 파일은 images/ 폴더에 넣어주세요
+
+const POSTS = [
+  {
+    thumb: "images/post1/thumb.jpg",
+    images: [
+      "images/post1/1.jpg",
+      "images/post1/2.jpg",
+      "images/post1/3.jpg"
+    ]
+  },
+  {
+    thumb: "images/post2/thumb.jpg",
+    images: [
+      "images/post2/1.jpg"
+    ]
+  },
+  // 게시글을 추가하려면 위 형식대로 복사해서 붙여넣으세요
+];
